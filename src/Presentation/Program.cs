85514f8add1fa1f6b9c4f755777ff4a72fc7387a
guillaume iter <guillaume.saintetienne@iter.org>
2023-12-06 17:15:15 +0100

    fix compile

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,26 +1,32 @@
 using CleanMinimalApi.Presentation.Extensions;
 using Serilog;
 
-var builder = WebApplication
-    .CreateBuilder(args)
-    .ConfigureApplicationBuilder();
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        var builder = WebApplication
+            .CreateBuilder(args)
+            .ConfigureApplicationBuilder();
 
-var app = builder
-    .Build()
-    .ConfigureApplication();
+        var app = builder
+            .Build()
+            .ConfigureApplication();
 
-try
-{
-    Log.Information("Starting host");
-    app.Run();
-    return 0;
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Host terminated unexpectedly");
-    return 1;
-}
-finally
-{
-    Log.CloseAndFlush();
+        try
+        {
+            Log.Information("Starting host");
+            app.Run();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 }
